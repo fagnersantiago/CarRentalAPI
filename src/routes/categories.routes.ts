7126c9a65be7ff8e50caa4aec50314477c28a07e
fagnersantiago <fagner.santiago@hotmail.com>
2021-03-26 14:55:30 -0300

    configuração docker, docker-composer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post('', (request, response) => {
+    console.log('funcionando');
     return createCategoryController.handle(request, response);
 });
 
